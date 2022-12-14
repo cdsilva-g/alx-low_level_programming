cea56a9931b72c8c0b31b53996d742d14801ed79
cdsilva-g <christinegathuu@gmail.com>
2022-12-14 06:21:49 -0800

    Fix 2

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (n = 48; n < 58; n++)
 	{
-	       	putchar(n);
+		putchar(n);
 		if (n != 57)
 		{
 			putchar(',');
@@ -20,4 +20,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-

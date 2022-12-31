772220e830f66b30ca142ed78cd1e19f6baf2716
cdsilva-g <christinegathuu@gmail.com>
2022-12-31 03:05:00 -0800

    Added times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,10 +21,10 @@ void print_times_table(int n)
 				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
-					_putchat(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((k + '0');
+					_putchar(' ');
+					_putchar(k + '0');
 				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
@@ -37,7 +37,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0'0);
+					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 			}

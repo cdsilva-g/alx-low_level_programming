d8ca530e90b0197c4c8555ffc0e5f000fd58b9fb
cdsilva-g <christinegathuu@gmail.com>
2022-12-31 03:08:40 -0800

    Added times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n >= 0 && n <=15)
+	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -32,7 +32,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
-				} else if ( k >= 100)
+				} else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');

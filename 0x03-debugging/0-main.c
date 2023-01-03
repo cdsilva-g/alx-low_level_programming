eb548a087e637b94066ff46427efba58a1811b5c
cdsilva-g <christinegathuu@gmail.com>
2023-01-03 02:38:32 -0800

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tets function that prints if integer is positive or nrgative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+	positive_or_negative(i);
+
+	return (0);
+}
